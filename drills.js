2c84c0a9b84037af2ecd1218b22d8b59bfe6b8ec
Leon Dail <leondailofficial@gmail.com>
2019-11-04 09:10:16 -0800

    first drill kinda completed

---
+++ drills.js
@@ -0,0 +1,11 @@
+// //Drill 1
+// function sheepCall(sheep) {
+//   if (sheep === 0) {
+//     return 'All sheep jumped over the fence';
+//   }
+//   return `${sheep}: Another sheep jumps over the fence` + sheepCall(sheep-1);
+// }
+// console.log(sheepCall(3));
+
+
+// Drill 2
